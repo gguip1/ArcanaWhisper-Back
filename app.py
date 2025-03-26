ba3c75aadb2ad8383213255577b93be3b8ddd558
gguip1 <51047947+gguip1@users.noreply.github.com>
2025-03-26 18:50:14 +0900

    v1.5:페이지 렌더 방식 변경, Gemini API 추가가

--- app.py
+++ app.py
@@ -1,35 +1,37 @@
 import streamlit as st
 
+from ui.common_style import common_style
+
 st.set_page_config(
     page_title='Arcana Whisper',
     page_icon='🔮',
-    layout='wide',
+    layout='centered',
+    initial_sidebar_state="collapsed"
 )
 
-from page import index, tarot, result
+# 스타일
+common_style()
 
-# params = st.query_params
-# page = params.get('page', 'index')
+# 타이틀, 소개
+st.markdown("<h1 class='centered'>🔮 Arcana Whisper</h1>", unsafe_allow_html=True)
+st.markdown("<h3 class='centered'>타로 카드와 LLM이 속삭이는 운명의 메시지</h3>", unsafe_allow_html=True)
 
-# def setPage(pageName):
-#     st.query_params['page'] = pageName
+# 설명
+st.markdown(
+    """
+    <div class='centered'>
+        <p><strong>Arcana Whisper</strong>는 인공지능을 통해 타로 카드 리딩을 경험할 수 있는 타로 서비스입니다.<br>
+        신비로운 타로의 세계와 인공지능의 힘이 만나, 지금 이 순간 당신에게 필요한 메시지를 전해드립니다.</p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
-if 'page' not in st.session_state:
-    st.session_state.page = 'index'
+st.warning(
+    "Arcana Whisper는 오락 및 자기 성찰용으로 제공됩니다.\n\n"
+    "- 실제 인생 결정은 전문가 상담 및 자신의 판단을 기반으로 하길 권장합니다.\n"
+    "- 이 앱은 사용자의 심리 상태를 분석하거나 예언하지 않습니다."
+)
 
-def setPage(pageName):
-    st.session_state.page = pageName
-    st.rerun()
-
-if st.session_state.page == 'index':
-    index.render(setPage)
-    st.stop()
-elif st.session_state.page == 'tarot':
-    tarot.render(setPage)
-    st.stop()
-elif st.session_state.page == 'result':
-    result.render(setPage)
-    st.stop()
-else:
-    index.render(setPage)
-    st.stop()
+if st.button("오늘의 타로 보기 ✨"):
+    st.switch_page('pages/tarot.py')
